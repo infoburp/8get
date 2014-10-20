a4b229866efea4b42dd1ab54b6d5ae00b9770178
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-20 03:52:36 +0100

    Update get.py

--- get.py
+++ get.py
@@ -1,12 +1,22 @@
 import urllib.request, urllib.parse, urllib.error, os, re
-url = raw_input('Enter base url, e.g. https://8chan.co/b/res/527104.html')
-	try:
-		urllib.request.urlretrieve(url,'base.html')
-		print(url)
-	except urllib.error.HTTPError as err:
-		print(err.code)
+url = input('Enter base url, e.g. https://8chan.co/b/res/527104.html (or return to use google.com)')
+if url == '':
+	url = 'http://google.com'
+i=1
+match = []
+try:
+	urllib.request.urlretrieve(url,'base.html')
+	print(url)
+except urllib.error.HTTPError as err:
+	print(err.code)
 with open('base.html') as html:
     content = html.read()
-    matches = re.findall(r'\ssrc="([^"]+)"', text)
-    matches = ' '.join(matches)
-print(matches)
+    matches = re.findall(r'\ssrc="([^"]+)"', content)
+for match in matches:
+	try:
+		split = urllib.parse.urlsplit(url+match)
+		filename = split.path.split("/")[-1]
+		urllib.request.urlretrieve(url+match, filename)
+		print(url+match)
+	except urllib.error.HTTPError as err:
+		print(err.code)
