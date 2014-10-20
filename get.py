f145e3e982a48219eff9e56e39ae29d1ffd43d6b
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-20 04:24:25 +0100

    Update get.py

--- get.py
+++ get.py
@@ -1,7 +1,12 @@
 import urllib.request, urllib.parse, urllib.error, os, re, glob
-url = input('Enter base url, e.g. https://8chan.co/b/res/527104.html (or return to use google.com)')
-if url == '':
-	url = 'http://google.com'
+board = input('Enter board e.g. "b":')
+if board=='':
+	board='b'
+thread = input('Enter thread e.g. "527104":')
+if thread=='':
+	thread='527104'
+thumbs = input('Download thumbs(return), or full(y, or anything else):')
+url='https://8chan.co/'+board+'/res/'+thread+'.html'
 i=1
 match = []
 try:
@@ -14,11 +19,20 @@ with open('base.html') as html:
     matches = re.findall(r'\ssrc="([^"]+)"', content)
 for match in matches:
 	if match.find(".js") == -1:
-		if match.find(".php") == -1:  
-			try:
-				split = urllib.parse.urlsplit('8chan.co'+match)
-				filename = split.path.split("/")[-1]
-				urllib.request.urlretrieve('http://8chan.co'+match, filename)
-				print(url+match)
-			except urllib.error.HTTPError as err:
-				print(err.code)
+		if match.find(".php") == -1:
+			if thumbs == '':
+				try:
+					split = urllib.parse.urlsplit('8chan.co'+match)
+					filename = split.path.split("/")[-1]
+					urllib.request.urlretrieve('http://8chan.co'+match, filename)
+					print(url+match)
+				except urllib.error.HTTPError as err:
+					print(err.code)
+			if thumbs != '':
+				try:
+					split = urllib.parse.urlsplit('8chan.co'+match.replace("thumb","src"))
+					filename = split.path.split("/")[-1]
+					urllib.request.urlretrieve('http://8chan.co'+match.replace("thumb","src"), filename)
+					print(url+match.replace("thumb","src"))
+				except urllib.error.HTTPError as err:
+					print(err.code)
