0a1ef02b235cde6d1984200a869abf02f8dbd740
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-20 04:10:13 +0100

    Update get.py

--- get.py
+++ get.py
@@ -1,4 +1,4 @@
-import urllib.request, urllib.parse, urllib.error, os, re
+import urllib.request, urllib.parse, urllib.error, os, re, glob
 url = input('Enter base url, e.g. https://8chan.co/b/res/527104.html (or return to use google.com)')
 if url == '':
 	url = 'http://google.com'
@@ -13,10 +13,12 @@ with open('base.html') as html:
     content = html.read()
     matches = re.findall(r'\ssrc="([^"]+)"', content)
 for match in matches:
-	try:
-		split = urllib.parse.urlsplit(url+match)
-		filename = split.path.split("/")[-1]
-		urllib.request.urlretrieve(url+match, filename)
-		print(url+match)
-	except urllib.error.HTTPError as err:
-		print(err.code)
+	if match.find(".js") == -1:
+		if match.find(".php") == -1:  
+			try:
+				split = urllib.parse.urlsplit('8chan.co'+match)
+				filename = split.path.split("/")[-1]
+				urllib.request.urlretrieve('http://8chan.co'+match, filename)
+				print(url+match)
+			except urllib.error.HTTPError as err:
+				print(err.code)
